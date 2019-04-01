ed9635cf9feda4fcd5af8a162a7da617a5767598
emmanuela <eoa22@njit.edu>
2019-04-01 17:24:52 -0400

    pushing to heroku

--- public/index.php
+++ public/index.php
@@ -6,46 +6,70 @@
  * Time: 2:35 AM
  */
 
+
 main::start("example.csv");
-class main{
-    static public function start($filename){
+class main  {
+    static public function start($filename) {
         $records = csv::getRecords($filename);
         $table = html::generateTable($records);
+        echo "$table";
     }
+
+
+
 }
-class html
-{
-    public static function generateTable($records)
-    {
+class html {
+    public static function generateTable($records) {
+        $count = 0;
+        $table = " ";
         foreach ($records as $record) {
+            $table .= "<html><head> <!-- Latest compiled and minified CSS -->
+<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">
+
+<!-- jQuery library -->
+<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script> </head><body><table class='table table-striped'>";
+
             if($count == 0) {
                 $array = $record->returnArray();
                 $fields = array_keys($array);
-                $values = array_values($array);
-                print_r($fields);
-                print_r($values);
-            } else{
+                $table .= "<tr>" ;
+                //"echo <tr>";
+                foreach($fields as $field)
+                {
+                    $table .= "<th>" . $field . "</th>";
+                    $count = 1 ;
+                }
+                $table .="</tr>";
                 $array = $record->returnArray();
                 $values = array_values($array);
-                print_r($values);
+                $table .="<tr>";
+                foreach($values as $value)
+                {
+                    $table .= "<td>" . $value . "</td>" ;
+                }
+                $table .="</tr>";
+
             }
-            $count++;
+            $table .="</table><body></html>";
+            return $table;
         }
     }
 }
 class csv {
     static public function getRecords($filename) {
-        $file = fopen($filename, "r");
-        $fieldNames =array();
+        $file = fopen($filename,"r");
+        $fieldNames = array();
         $count = 0;
         while(! feof($file))
         {
             $record = fgetcsv($file);
-            if($count == 0){
+            if($count == 0) {
                 $fieldNames = $record;
-            }
-            else{
-                $records[] = recordFactory::create($fieldNames,$record);
+            } else {
+                $records[] = recordFactory::create($fieldNames, $record);
             }
             $count++;
         }
@@ -54,22 +78,23 @@ class csv {
     }
 }
 class record {
-    public function __construct(Array $fieldNames = null, $values = null)
+    public function __construct(Array $fieldNames = null, $values = null )
     {
         $record = array_combine($fieldNames, $values);
         foreach ($record as $property => $value) {
             $this->createProperty($property, $value);
         }
     }
-    public function returnArray () {
+    public function returnArray() {
         $array = (array) $this;
         return $array;
     }
-    public function createProperty($name = 'first', $value ='Emmanuel'){
+    public function createProperty($name = 'first', $value = 'italo') {
         $this->{$name} = $value;
     }
 }
 class recordFactory {
+
     public static function create(Array $fieldNames = null, Array $values = null) {
         $record = new record($fieldNames, $values);
         return $record;
